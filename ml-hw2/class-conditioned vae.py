95c305a51688d88ce33f2f87835689cb7a5aee91
ZhikunWei <1045525044@qq.com>
2019-11-13 22:32:28 +0800

    hw2, vae, to generate mnist digits

--- ml-hw2/class-conditioned vae.py
+++ ml-hw2/class-conditioned vae.py
@@ -61,11 +61,13 @@ def save_image_collections(x, filename, shape=(10, 10), scale_each=False,
 
 
 @zs.meta_bayesian_net(scope="gen", reuse_variables=True)
-def build_gen(x_dim, z_dim, n, n_particles=1):
+def build_gen(x_dim, z_dim, y, n, n_particles=1):
     bn = zs.BayesianNet()
     z_mean = tf.zeros([n, z_dim])
     z = bn.normal("z", z_mean, std=1., group_ndims=1, n_samples=n_particles)
-    h = tf.layers.dense(z, 500, activation=tf.nn.relu)
+    y = tf.reshape(y, [1, tf.shape(y)[0], 10])
+    y = tf.tile(y, [n_particles, 1, 1])
+    h = tf.layers.dense(tf.concat([y, z], axis=2), 500, activation=tf.nn.relu)
     h = tf.layers.dense(h, 500, activation=tf.nn.relu)
     x_logits = tf.layers.dense(h, x_dim)
     bn.deterministic("x_mean", tf.sigmoid(x_logits))
@@ -74,9 +76,9 @@ def build_gen(x_dim, z_dim, n, n_particles=1):
 
 
 @zs.reuse_variables(scope="q_net")
-def build_q_net(x, z_dim, n_z_per_x):
+def build_q_net(x, z_dim, y, n_z_per_x):
     bn = zs.BayesianNet()
-    h = tf.layers.dense(tf.cast(x, tf.float32), 500, activation=tf.nn.relu)
+    h = tf.layers.dense(tf.concat([tf.cast(x, tf.float32), y], axis=-1), 500, activation=tf.nn.relu)
     h = tf.layers.dense(h, 500, activation=tf.nn.relu)
     z_mean = tf.layers.dense(h, z_dim)
     z_logstd = tf.layers.dense(h, z_dim)
@@ -89,18 +91,20 @@ def main():
     # the file comes from http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
     x_train, t_train, x_valid, t_valid, x_test, t_test = load_mnist(data_path)
     x_train = np.vstack([x_train, x_valid])
+    y_train = np.vstack([t_train, t_valid])
     x_test = np.random.binomial(1, x_test, size=x_test.shape)
     x_dim = x_train.shape[1]
-
+    y_dim = y_train.shape[1]
     # Define model parameters
     z_dim = 40
     n_particles = tf.placeholder(tf.int32, shape=[], name="n_particles")
     x_input = tf.placeholder(tf.float32, shape=[None, x_dim], name="x")
+    y = tf.placeholder(tf.float32, shape=[None, y_dim], name="y")
     x = tf.cast(tf.less(tf.random_uniform(tf.shape(x_input)), x_input), tf.int32)
     n = tf.placeholder(tf.int32, shape=[], name="n")
 
-    model = build_gen(x_dim, z_dim, n, n_particles)
-    variational = build_q_net(x, z_dim, n_particles)
+    model = build_gen(x_dim, z_dim, y, n, n_particles)
+    variational = build_q_net(x, z_dim, y, n_particles)
 
     lower_bound = zs.variational.elbo(
         model, {"x": x}, variational=variational, axis=0)
@@ -115,7 +119,8 @@ def main():
     infer_op = optimizer.minimize(cost)
 
     # Random generation
-    x_gen = tf.reshape(model.observe()["x_mean"], [-1, 28, 28, 1])
+    y_observe = tf.placeholder(tf.float32, shape=[None, 10], name="y_obs")
+    x_gen = tf.reshape(model.observe(y=y_observe)["x_mean"], [-1, 28, 28, 1])
 
     # Define training/evaluation parameters
     epochs = 3000
@@ -125,7 +130,7 @@ def main():
     test_freq = 10
     test_batch_size = 20
     test_iters = x_test.shape[0] // test_batch_size
-    result_path = "results/vae"
+    result_path = "results/c-vae"
 
     # Run the inference
     with tf.Session() as sess:
@@ -137,8 +142,10 @@ def main():
             lbs = []
             for t in range(iters):
                 x_batch = x_train[t * batch_size:(t + 1) * batch_size]
+                y_batch = y_train[t * batch_size:(t + 1) * batch_size]
                 _, lb = sess.run([infer_op, lower_bound],
                                  feed_dict={x_input: x_batch,
+                                            y: y_batch,
                                             n_particles: 1,
                                             n: batch_size})
                 lbs.append(lb)
@@ -150,14 +157,16 @@ def main():
                 time_test = -time.time()
                 test_lbs, test_lls = [], []
                 for t in range(test_iters):
-                    test_x_batch = x_test[t * test_batch_size:
-                                          (t + 1) * test_batch_size]
+                    test_x_batch = x_test[t * test_batch_size:(t + 1) * test_batch_size]
+                    test_y_batch = t_test[t * test_batch_size:(t + 1) * test_batch_size]
                     test_lb = sess.run(lower_bound,
                                        feed_dict={x: test_x_batch,
+                                                  y: test_y_batch,
                                                   n_particles: 1,
                                                   n: test_batch_size})
                     test_ll = sess.run(is_log_likelihood,
                                        feed_dict={x: test_x_batch,
+                                                  y: test_y_batch,
                                                   n_particles: 1000,
                                                   n: test_batch_size})
                     test_lbs.append(test_lb)
@@ -165,13 +174,13 @@ def main():
                 time_test += time.time()
                 print(">>> TEST ({:.1f}s)".format(time_test))
                 print(">> Test lower bound = {}".format(np.mean(test_lbs)))
-                print('>> Test log likelihood (IS) = {}'.format(
-                    np.mean(test_lls)))
+                print('>> Test log likelihood (IS) = {}'.format(np.mean(test_lls)))
 
             if epoch % save_freq == 0:
-                images = sess.run(x_gen, feed_dict={n: 100, n_particles: 1})
-                name = os.path.join(result_path,
-                                    "vae.epoch.{}.png".format(epoch))
+                y_index = np.repeat(np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), 10)
+                y_index = np.eye(10)[y_index.reshape(-1)]
+                images = sess.run(x_gen, feed_dict={y_observe: y_index, y: y_index, n: 100, n_particles: 1})
+                name = os.path.join(result_path, "vae.epoch.{}.png".format(epoch))
                 save_image_collections(images, name)
 
 
